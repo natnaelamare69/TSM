01ce3b544f5307452fbf0241dc63886a1361dcbe
Natnael Amare <etanright@gmail.com>
2025-08-06 23:12:09 +0300

    i did plotting of the city list

---
+++ city_plot.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+import json
+
+data = open('city_list.json')
+fromjson = json.load(data)
+cities = fromjson["cities"]
+
+lats = [float(city[1]) for city in cities]
+longs = [float(city[2]) for city in cities]
+names = [city[0] for city in cities]
+
+#normalizing 
+min_lat, max_lat = min(lats), max(lats)
+min_lon, max_lon = min(longs), max(longs)
+
+norm_lats = [(lat - min_lat) / (max_lat - min_lat) for lat in lats]
+norm_longs = [(lon - min_lon) / (max_lon - min_lon) for lon in longs]
+
+
+plt.figure(figsize=(10, 10))
+for i, name in enumerate(names):
+    plt.text(norm_longs[i], norm_lats[i], name, fontsize=7)
+
+plt.scatter(norm_longs, norm_lats, color='red')
+plt.title("travelling sales man probelem: ethiopian cities")
+plt.show()
+
+data.close()
